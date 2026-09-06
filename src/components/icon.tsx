d2f5ff535eab29c24fc07cdc3657b17067764b5c
FernandoHN99 <nandohneto@gmail.com>
2026-09-05 22:09:55 -0300

    fix: db.josn and icon

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -12,11 +12,7 @@ export const iconVariants = cva('', {
    },
 })
 
-interface IconProps
-   extends React.ComponentProps<'svg'>,
-      VariantProps<typeof iconVariants> {
-   svg: React.FC<React.ComponentProps<'svg'>>
-}
+interface IconProps extends React.ComponentProps<'svg'>, VariantProps<typeof iconVariants> { svg: React.FC<React.ComponentProps<'svg'>>}
 
 function Icon({ svg: SvgComponent, animate, className, ...props }: IconProps) {
    return (
